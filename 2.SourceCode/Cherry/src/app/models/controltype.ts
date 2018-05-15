fae3f391f6506c86bf3cbb423825951b74dfdccf
Chen Dong <dchen15@dow.com>
2018-05-15 16:56:22 +0800

    dynamic load controls

---
+++ 2.SourceCode/Cherry/src/app/models/controltype.ts
@@ -0,0 +1,4 @@
+export class ControlType {
+  public Name: string;
+
+}
--- 2.SourceCode/Cherry/src/app/web-designer/control-set/control-set.component.ts
+++ 2.SourceCode/Cherry/src/app/web-designer/control-set/control-set.component.ts
@@ -1,15 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import { ControlType } from '../../models/controltype';
 
 @Component({selector: 'app-control-set', templateUrl: './control-set.component.html', styleUrls: ['./control-set.component.css']})
 export class ControlSetComponent implements OnInit {
 
+  Controls: ControlType[] = [];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.Controls.push({Name: 'Question'});
+    this.Controls.push({Name: 'Answer-Radio'});
+    this.Controls.push({Name: 'Answer-Text'});
+    this.Controls.push({Name: 'Answer-CheckBox'});
+  }
   onDragStart(evt: any) {
     // evt.preventDefault();
-    console.log('drag start');
+    const ctrlName = evt.target.innerText;
+    console.log(ctrlName + ' drag start');
     evt.dataTransfer.dropEffect = 'move';
-    evt.dataTransfer.setData('text/plain', 'an control');
+    evt
+      .dataTransfer
+      .setData('text/plain', ctrlName);
   }
 }
